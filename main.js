55ef1d24d567a4907eb02e7d636805db6d84302e
Uloma07 <uloma07@users.noreply.github.com>
2016-09-23 16:39:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
 	
     // Your code here.
+   var theeditrow = null;
     
     $('#sandbox-container input').datepicker({
 });
@@ -85,6 +86,31 @@ $( document ).ready(function() {
         $(this).parent().parent().remove();
 	});
 	
+	$("#save").click(function(){
+		var examtype = $('#edita').find('option:selected').text();
+		var examdate ="";
+		if($('#NoDateModal').is(':checked')){
+		examdate = "No date listed";
+		}
+		else{
+			examdate = $('#editc').val();
+		}
+		var examstyle = $('#editb').find('option:selected').text();
+		
+		var row = '<tr><td>'+examtype+'</td><td>'+examstyle + '</td><td>'+examdate + '</td><td><input type="button" class="removerow" value="Remove">&nbsp&nbsp<input type="button" class="editrow" value="Edit"></td></tr>';
+		if(examdate==''){
+			$('#editc').addClass("error").attr('title', 'An error occurred!');
+			//alert('nothing in here')
+		}
+		else{
+			theeditrow = row;
+			$('#editc').removeClass("error");
+			$("#editc").removeAttr("title");
+			$('#editc').val('');
+			$('#NoDateModal').attr('checked', false); // Unchecks it
+		}
+	});
+	
 	$("#ExamStuff").on('click','.editrow',function(){
         var therow = $(this).parent().parent();
 	        var type= therow.find("td").eq(0).html();
@@ -92,8 +118,14 @@ $( document ).ready(function() {
 	        var date= therow.find("td").eq(2).html();
 	        $("#editModal").find("#edita").val(type);
 	        $("#editModal").find("#editb").val(style);
+	        if(date == "No date listed"){
+	        	$("#editModal").find("#editc").attr("disabled", "disabled");
+	         	$("#editModal").find('#NoDateModal').attr('checked', true);
+	        }else{
 	        $("#editModal").find("#editc").val(date);
+	        }
 	        //var date= therow.find("td").eq(2).html();
+	        theeditrow = therow;
 	        $("#editModal").modal();
 	        //alert(date);
 	});
